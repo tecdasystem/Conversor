6dcbd63c2de54a310179ed6d146e87dd5ad14287
Remesas-01 <Remesas-01@172.16.0.52>
2023-04-15 10:39:28 -0300

    actualización de tasas de cambio de las monedas, nombre de los pesos colombianos a argentinos

--- Conversor/src/conversorMonedas/ConvertirMonedas.java
+++ Conversor/src/conversorMonedas/ConvertirMonedas.java
@@ -6,31 +6,31 @@ public class ConvertirMonedas {
 
 	
 	public void ConvertirPesosADolares(double valor) {
-		double monedaDolar = valor / 3739.00;
+		double monedaDolar = valor / 400.00;
 		monedaDolar = (double) Math.round(monedaDolar *100d)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Dolares");
 	}
 	
 	public void ConvertirPesosAEuros(double valor) {
-		double monedaEuro = valor / 4050.48;
+		double monedaEuro = valor / 240.87;
 		monedaEuro = (double) Math.round(monedaEuro *100d)/100;
 		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Euros");
 	}
 	
 	public void ConvertirPesosALibras(double valor) {
-		double monedaLibra = valor / 4890.52;
+		double monedaLibra = valor / 266.87;
         monedaLibra = (double) Math.round(monedaLibra *100d)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Libras Esterlinas");
 	}
 	
 	public void ConvertirPesosAYen(double valor) {
-		double monedaYen = valor / 29.68;
+		double monedaYen = valor / 31.30;
         monedaYen = (double) Math.round(monedaYen *100d)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Yuanes");
 	}
 	
 	public void ConvertirPesosAWon(double valor) {
-		double monedaWon = valor / 3.04;
+		double monedaWon = valor / 6.16;
         monedaWon = (double) Math.round(monedaWon *100d)/100;
         JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Wons");
 	}
--- Conversor/src/conversorMonedas/ConvertirMonedasAPesos.java
+++ Conversor/src/conversorMonedas/ConvertirMonedasAPesos.java
@@ -4,32 +4,32 @@ import javax.swing.JOptionPane;
 
 public class ConvertirMonedasAPesos {
 	public void ConvertirDolaresAPesos(double valor) {
-		double monedaDolar = valor * 3739.00;
+		double monedaDolar = valor * 400.00;
 		monedaDolar = (double) Math.round(monedaDolar *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Pesos Argentinos");
 	}
 	
 	public void ConvertirEurosAPesos(double valor) {
-		double monedaEuro = valor * 4050.48;
+		double monedaEuro = valor * 240.87;
 		monedaEuro = (double) Math.round(monedaEuro *100d)/100;
-		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Pesos Colombianos");
+		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Pesos Argentinos");
 	}
 	
 	public void ConvertirLibrasAPesos(double valor) {
-		double monedaLibra = valor * 4890.52;
+		double monedaLibra = valor * 266.87;
         monedaLibra = (double) Math.round(monedaLibra *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Pesos Argentinos");
 	}
 	
 	public void ConvertirYenAPesos(double valor) {
-		double monedaYen = valor * 29.68;
+		double monedaYen = valor * 31.30;
         monedaYen = (double) Math.round(monedaYen *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Pesos Argentinos");
 	}
 	
 	public void ConvertirWonAPesos(double valor) {
-		double monedaWon = valor * 3.04;
+		double monedaWon = valor * 6.16;
         monedaWon = (double) Math.round(monedaWon *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Pesos Argentinos");
 	}
 }
